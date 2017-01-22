08b902b54092d6e060265669391c8bd2f181f366
andy <andy@andydote.co.uk>
2017-01-22 18:28:39 +0200

    splice: basic command structure and finder

---
+++ src/NuSplice/ICommand.cs
@@ -0,0 +1,12 @@
+﻿namespace NuSplice
+{
+	public interface ICommand
+	{
+		void Execute(CommandModel model);
+	}
+
+	public class CommandModel
+	{
+		public string[] Args { get; set; }
+	}
+}
---
+++ src/NuSplice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace NuSplice
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			var commandInterface = typeof(ICommand);
+			var commands = commandInterface
+				.Assembly
+				.GetTypes()
+				.Where(t => t.IsClass && t.IsAbstract == false && commandInterface.IsAssignableFrom(t))
+				.Where(t => t.GetConstructor(Type.EmptyTypes) != null);
+
+			var type = commands.First(t => t.Name.StartsWith(args[0]));
+
+			var command = (ICommand)type.GetConstructor(Type.EmptyTypes).Invoke(new object[0]);
+
+			command.Execute(new CommandModel
+			{
+				Args = args
+			});
+		}
+	}
+}
