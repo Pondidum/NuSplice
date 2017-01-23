20f5fe841e46eda63f19a3b2ba2dcded2016d3c2
andy <andy@andydote.co.uk>
2017-01-23 22:27:49 +0200

    commandLocator: extract command location, add tests

---
+++ src/NuSplice.Tests/CommandLocatorTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+
+namespace NuSplice.Tests
+{
+	public class CommandLocatorTests
+	{
+		private IEnumerable<Type> ForTypes(params Type[] types) => new CommandLocator(types).Execute();
+
+		[Fact]
+		public void When_scanning_against_no_types()
+		{
+			ForTypes().ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_scanning_types_and_none_implement_the_interface()
+		{
+			ForTypes(typeof(object)).ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_scanning_and_one_type_implements_the_interface()
+		{
+			ForTypes(typeof(ImplementsConstructable)).ShouldBe(new[] { typeof(ImplementsConstructable) });
+		}
+
+		[Fact]
+		public void When_one_type_implements_the_interface_but_has_no_ctor()
+		{
+			ForTypes(typeof(ImplementsNonConstructable)).ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_one_type_implements_the_interface_but_is_abstract()
+		{
+			ForTypes(typeof(ImplementsAbstract)).ShouldBeEmpty();
+		}
+
+		private class ImplementsConstructable : ICommand
+		{
+			public void Execute(CommandModel model)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		private class ImplementsNonConstructable : ICommand
+		{
+			private ImplementsNonConstructable()
+			{
+			}
+
+			public void Execute(CommandModel model)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		private abstract class ImplementsAbstract : ICommand
+		{
+			public void Execute(CommandModel model)
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
---
+++ src/NuSplice/CommandLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NuSplice
+{
+	public class CommandLocator
+	{
+		private readonly Type[] _types;
+
+		public CommandLocator()
+			: this(typeof(ICommand).Assembly.GetTypes())
+		{
+		}
+
+		public CommandLocator(Type[] types)
+		{
+			_types = types;
+		}
+
+		public IEnumerable<Type> Execute()
+		{
+			var commandInterface = typeof(ICommand);
+
+			return _types
+				.Where(t => t.IsClass && t.IsAbstract == false && commandInterface.IsAssignableFrom(t))
+				.Where(t => t.GetConstructor(Type.EmptyTypes) != null);
+		}
+	}
+}
