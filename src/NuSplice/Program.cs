54bdfbcc9461a7b44336e9d7ce3cae94b415fe20
andy <andy@andydote.co.uk>
2017-01-24 22:18:08 +0200

    program: use the commandLocator

--- src/NuSplice/Program.cs
+++ src/NuSplice/Program.cs
@@ -7,20 +7,15 @@ namespace NuSplice
 	{
 		public static void Main(string[] args)
 		{
-			var commandInterface = typeof(ICommand);
-			var commands = commandInterface
-				.Assembly
-				.GetTypes()
-				.Where(t => t.IsClass && t.IsAbstract == false && commandInterface.IsAssignableFrom(t))
-				.Where(t => t.GetConstructor(Type.EmptyTypes) != null);
+			var locator = new CommandLocator();
+			var commands = locator.Execute();
 
 			var type = commands.First(t => t.Name.StartsWith(args[0]));
-
 			var command = (ICommand)type.GetConstructor(Type.EmptyTypes).Invoke(new object[0]);
 
 			command.Execute(new CommandModel
 			{
-				Args = args
+				Args = args.Skip(1).ToArray()
 			});
 		}
 	}
