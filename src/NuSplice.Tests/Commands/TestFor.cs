c47805ef0982f13adcdd78489fa9b9390730a9aa
andy <andy@andydote.co.uk>
2017-01-25 23:52:54 +0200

    testFor: add convienience methods for testing with

--- src/NuSplice.Tests/Commands/TestFor.cs
+++ src/NuSplice.Tests/Commands/TestFor.cs
@@ -1,4 +1,7 @@
-﻿namespace NuSplice.Tests.Commands
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NuSplice.Tests.Commands
 {
 	public abstract class TestFor<TCommand> where TCommand : Command, new()
 	{
@@ -7,6 +10,17 @@
 		protected TestFor()
 		{
 			Command = new TCommand();
+		}
+
+		protected IEnumerable<IEvent> Events => Command.GetPendingEvents();
+		protected IEvent SingleEvent => Command.GetPendingEvents().Single();
+
+		protected void Execute(params string[] args)
+		{
+			Command.Execute(new CommandModel
+			{
+				Args = args
+			});
 		}
 	}
 }
