d50d3d1dc6f6fc812fc2e9b40095135aa07d4975
andy <andy@andydote.co.uk>
2017-01-26 00:08:15 +0200

    installCommand: stub out argument parsing structure

--- src/NuSplice/Commands/InstallCommand.cs
+++ src/NuSplice/Commands/InstallCommand.cs
@@ -2,9 +2,38 @@
 {
 	public class InstallCommand : Command
 	{
+		public InstallCommand()
+		{
+			Register<InstallArgumentsParsed>(Handle);
+		}
+
 		public override void Execute(CommandModel model)
 		{
 			throw new System.NotImplementedException();
 		}
+
+		private void Handle(InstallArgumentsParsed e)
+		{
+			//?
+		}
+	}
+
+	public class InstallArgumentsParsed : IEvent
+	{
+		public InstallOptions Options { get; }
+
+		public InstallArgumentsParsed(InstallOptions options)
+		{
+			Options = options;
+		}
+	}
+
+	public class InstallOptions
+	{
+		[Option("name", Required = true)]
+		public string PackageName { get; set; }
+
+		[Option("version", Required = true)]
+		public string PackageVersion { get; set; }
 	}
 }
